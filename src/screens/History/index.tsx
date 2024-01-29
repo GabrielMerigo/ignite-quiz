29321aa37d5377c68341f76ec2082137195477c9
Gabriel <gabriel.merigo.dev@gmail.com>
2024-01-28 22:09:30 -0300

    finished animations

--- src/screens/History/index.tsx
+++ src/screens/History/index.tsx
@@ -9,6 +9,7 @@ import { HistoryCard, HistoryProps } from '../../components/HistoryCard';
 import { styles } from './styles';
 import { historyGetAll, historyRemove } from '../../storage/quizHistoryStorage';
 import { Loading } from '../../components/Loading';
+import Animated, { Layout, SlideOutRight } from 'react-native-reanimated';
 
 export function History() {
   const [isLoading, setIsLoading] = useState(true);
@@ -65,12 +66,13 @@ export function History() {
       >
         {
           history.map((item) => (
-            <TouchableOpacity
-              key={item.id}
-              onPress={() => handleRemove(item.id)}
-            >
-              <HistoryCard data={item} />
-            </TouchableOpacity>
+            <Animated.View exiting={SlideOutRight} key={item.id} layout={Layout.springify()}>
+              <TouchableOpacity
+                onPress={() => handleRemove(item.id)}
+              >
+                <HistoryCard data={item} />
+              </TouchableOpacity>
+            </Animated.View>
           ))
         }
       </ScrollView>

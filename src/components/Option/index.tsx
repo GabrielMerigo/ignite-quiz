9ddacacf95e360b1f54eb367a06edd73e88b5fb8
Gabriel <gabriel.merigo.dev@gmail.com>
2024-02-02 16:58:57 -0300

    adding option animated

--- src/components/Option/index.tsx
+++ src/components/Option/index.tsx
@@ -3,11 +3,17 @@ import { TouchableOpacity } from 'react-native-gesture-handler';
 import { 
   Canvas,
   Path,
-  Skia
+  Skia,
+  useValue,
+  runTiming,
+  BlurMask,
+  Circle
 } from '@shopify/react-native-skia';
 
 import { styles } from './styles';
 import { THEME } from '../../styles/theme';
+import { useEffect } from 'react';
+import { Easing } from 'react-native-reanimated';
 
 type OptionProps = {
   checked: boolean;
@@ -18,10 +24,24 @@ const CHECK_SIZE = 28;
 const CHECK_STROKE = 2;
 
 export function Option({ checked, title }: OptionProps) {
+  const percentage = useValue(0);
+  const circle = useValue(0);
+
   const RADIUS = (CHECK_SIZE - CHECK_STROKE) / 2;
+  const CENTER_CIRCLE = RADIUS / 2;
 
   const path = Skia.Path.Make();
   path.addCircle(CHECK_SIZE, CHECK_SIZE, RADIUS)
+
+  useEffect(() => {
+    if(checked){
+      runTiming(percentage, 1, {  duration: 500 });
+      runTiming(circle, CENTER_CIRCLE, {  easing: Easing.bounce });
+    }else{
+      runTiming(percentage, 0, {  duration: 300 });
+      runTiming(circle, 0, { duration: 300 });
+    }
+  }, [checked])
 
   return (
     <TouchableOpacity
@@ -50,7 +70,19 @@ export function Option({ checked, title }: OptionProps) {
           color={THEME.COLORS.BRAND_LIGHT} 
           style="stroke"
           strokeWidth={CHECK_STROKE} 
-        />
+          end={percentage}
+        >
+          <BlurMask blur={1} style="solid" />
+        </Path>
+
+        <Circle
+          cx={CHECK_SIZE}
+          cy={CHECK_SIZE}
+          r={circle}
+          color={THEME.COLORS.BRAND_LIGHT}
+        >
+          <BlurMask blur={4}  style="solid"/>
+        </Circle>
       </Canvas>
     </TouchableOpacity>
   );
